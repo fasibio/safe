f591cc510ffe9ad063ecc4e347b9da333ea47443
Fabian Simon <fabian.simon@inovex.de>
2025-02-14 05:57:59 +0100

    added comments

--- option.go
+++ option.go
@@ -7,10 +7,12 @@ import (
 
 var ErrValueIsNil = errors.New("Value is nil")
 
+// Wrapper around potenzial nil Values. Force nil checks before T can be used.
 type Option[T any] struct {
 	value *T
 }
 
+// Some creates an option of type T could nil.
 func Some[T any](value *T) Option[T] {
 	if value == nil {
 		return None[T]()
@@ -18,18 +20,22 @@ func Some[T any](value *T) Option[T] {
 	return Option[T]{value: value}
 }
 
+// None create a None Option by given T.
 func None[T any]() Option[T] {
 	return Option[T]{value: nil}
 }
 
+// IsSome returns true if the Option contains a value.
 func (o Option[T]) IsSome() bool {
 	return o.value != nil
 }
 
+// IsNone returns true if the Option is empty.
 func (o Option[T]) IsNone() bool {
 	return !o.IsSome()
 }
 
+// Some returns the value and true if Option is not empty, otherwise nil and false.
 func (o Option[T]) Some() (*T, bool) {
 
 	if o.IsSome() {
@@ -38,10 +44,12 @@ func (o Option[T]) Some() (*T, bool) {
 	return nil, false
 }
 
+// Unwrap returns the contained value without a nil check (use with caution).
 func (o Option[T]) Unwrap() *T {
 	return o.value
 }
 
+// SomeOrDefault returns the value if present, otherwise returns the provided default value.
 func (o Option[T]) SomeOrDefault(value *T) *T {
 	if result, ok := o.Some(); ok {
 		return result
@@ -49,6 +57,7 @@ func (o Option[T]) SomeOrDefault(value *T) *T {
 	return value
 }
 
+// SomeOrDefaultFn returns the value if present, otherwise calls the provided function to generate a default.
 func (o Option[T]) SomeOrDefaultFn(fn func() *T) *T {
 	if result, ok := o.Some(); ok {
 		return result
@@ -56,6 +65,7 @@ func (o Option[T]) SomeOrDefaultFn(fn func() *T) *T {
 	return fn()
 }
 
+// SomeOrError returns the value if present, otherwise returns the provided error.
 func (o Option[T]) SomeOrError(e error) (*T, error) {
 	if v, ok := o.Some(); ok {
 		return v, nil
@@ -63,18 +73,21 @@ func (o Option[T]) SomeOrError(e error) (*T, error) {
 	return nil, e
 }
 
+// SomeAndThen calls the provided function if Option contains a value.
 func (o Option[T]) SomeAndThen(fn func(value *T)) {
 	if v, ok := o.Some(); ok {
 		fn(v)
 	}
 }
 
+// NoneAndThen calls the provided function if Option is empty.
 func (o Option[T]) NoneAndThen(fn func()) {
 	if o.IsNone() {
 		fn()
 	}
 }
 
+// CopyOrDefault returns a copy of the value if present, otherwise returns the default value.
 func (o Option[T]) CopyOrDefault(defaultValue T) T {
 	if v, ok := o.Some(); ok {
 		return *v
@@ -82,6 +95,7 @@ func (o Option[T]) CopyOrDefault(defaultValue T) T {
 	return defaultValue
 }
 
+// CopySome returns a copy of the value and true if present, otherwise a zero-value and false.
 func (o Option[T]) CopySome() (T, bool) {
 	if v, ok := o.Some(); ok {
 		return *v, true
@@ -90,6 +104,7 @@ func (o Option[T]) CopySome() (T, bool) {
 	return d, false
 }
 
+// Transforms an Option[T] to an Option[P] using the provided function.
 func SomeAndMap[T any, P any](o Option[T], fn func(*T) Option[P]) Option[P] {
 	if v, ok := o.Some(); ok {
 		return fn(v)
--- ptr.go
+++ ptr.go
@@ -1,9 +1,11 @@
 package safe
 
+// Ptr returns a pointer of T.
 func Ptr[T any](v T) *T {
 	return &v
 }
 
+// SomePtr returns an option of  pointer of T.
 func SomePtr[T any](v T) Option[T] {
 	return Some(Ptr(v))
 }
